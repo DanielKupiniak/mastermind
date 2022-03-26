4fddf776b43b32fbe08e42e2cfc796ad8e751752
Daniel Kupiniak <daniel.kupiniak@gmail.com>
2022-03-26 18:36:33 +0100

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <vector>
+
+int main(int, char**) {
+  std::cout << "Hello, world!\n";
+  std::vector<int> a;
+}
---
+++ tests/test_mastermind.cxx
@@ -0,0 +1,4 @@
+#include <gtest/gtest.h>
+
+// Demonstrate some basic assertions.
+TEST(MasterMind, BasicAssertions) { EXPECT_EQ(1, 2); }
